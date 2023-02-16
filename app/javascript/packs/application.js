a378b87fbce95cbc8ea442339c9889edb1e65ea3
oneonetaiwan <tkci10.pooh@gmail.com>
2023-02-16 08:46:00 +0000

    [add]カリキュラム2章まで

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+let i;
+let num = 0;
+
+for(i = 2; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
