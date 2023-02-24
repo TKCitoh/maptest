eb6dbe40995d7b4cca90c2bc82680567d69350ba
oneonetaiwan <tkci10.pooh@gmail.com>
2023-02-24 11:42:49 +0000

    [add]chiderenとthis7章

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,14 +10,9 @@ import "channels"
 
 require('jquery')
 
-
-
-$(function (){
-  $('.box1').on('click', function (){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function (){
-    $('.box1').removeClass('box1-ext');
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
 
