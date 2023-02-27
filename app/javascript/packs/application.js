8abd499501e61e1bdddedc9f24f0b6f6a82ef5cf
oneonetaiwan <tkci10.pooh@gmail.com>
2023-02-27 06:42:07 +0000

    [fix]タブメニュー（jquery反映されず）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,16 +8,19 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-
 require('jquery')
 
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
+
 
 Rails.start()
 Turbolinks.start()
